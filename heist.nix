6bc2ddd7a355f7b2e7fc564868baf8816db79c2e
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-08 15:23:03 -0700

    rebuilt nix file

--- heist.nix
+++ heist.nix
@@ -1,18 +1,37 @@
-{ mkDerivation, aeson, attoparsec, base, blaze-builder, blaze-html
-, bytestring, containers, directory, directory-tree, dlist
-, filepath, hashable, lifted-base, map-syntax, monad-control, mtl
-, process, random, stdenv, text, time, transformers
+{ mkDerivation, aeson, attoparsec, base, bifunctors, blaze-builder
+, blaze-html, bytestring, containers, criterion, directory
+, directory-tree, dlist, errors, filepath, hashable, HUnit, lens
+, lifted-base, map-syntax, monad-control, mtl, process, QuickCheck
+, random, statistics, stdenv, test-framework, test-framework-hunit
+, test-framework-quickcheck2, text, time, transformers
 , transformers-base, unordered-containers, vector, xmlhtml
 }:
 mkDerivation {
   pname = "heist";
   version = "1.0.0.0";
   src = ./.;
+  isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
     aeson attoparsec base blaze-builder blaze-html bytestring
     containers directory directory-tree dlist filepath hashable
     lifted-base map-syntax monad-control mtl process random text time
     transformers transformers-base unordered-containers vector xmlhtml
+  ];
+  executableHaskellDepends = [
+    aeson attoparsec base blaze-builder blaze-html bytestring
+    containers criterion directory directory-tree dlist errors filepath
+    hashable HUnit lifted-base map-syntax monad-control mtl process
+    random statistics test-framework test-framework-hunit text time
+    transformers transformers-base unordered-containers vector xmlhtml
+  ];
+  testHaskellDepends = [
+    aeson attoparsec base bifunctors blaze-builder blaze-html
+    bytestring containers directory directory-tree dlist errors
+    filepath hashable HUnit lens lifted-base map-syntax monad-control
+    mtl process QuickCheck random test-framework test-framework-hunit
+    test-framework-quickcheck2 text time transformers transformers-base
+    unordered-containers vector xmlhtml
   ];
   homepage = "http://snapframework.com/";
   description = "An Haskell template system supporting both HTML5 and XML";
